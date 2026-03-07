0dca2d0f475356a72125ed94048b480ad319c2a1
mlplmkokn-cell <mlplmkokn@gmail.com>
2026-03-07 12:22:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -13,19 +13,12 @@ load_dotenv()
 
 # --- КОНФИГУРАЦИЯ ---
 BOT_TOKEN = os.getenv('BOT_TOKEN')
-
 SHOP_ID = os.getenv('SHOP_ID')
-
 SHOP_API_KEY = os.getenv('SHOP_API_KEY')
-
 FREE_GEMINI_KEY = os.getenv('FREE_GEMINI_KEY')
-
 VIP_GEMINI_KEY = os.getenv('VIP_GEMINI_KEY')
-
 BOT_USERNAME = os.getenv('BOT_USERNAME')
-
 DATABASE_URL = os.getenv('DATABASE_URL')
-
 MAX_RPM = 15 # Лимит запросов в минуту
 
 Configuration.configure(SHOP_ID, SHOP_API_KEY)
